f789edac7b1b719e1ecd83416746598991198c20
Korcsmáros Kristóf <korcsmaros20022002@gmail.com>
2020-02-17 09:34:07 +0100

    Feladatok

---
+++ ConsoleApp1/ConsoleApp1/Auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Auto
+    {
+        public static List<Auto> lista = new List<Auto>();
+        private string márka;
+        private string model;
+        private int evJarat;
+        private int km;
+        private string uzemAnyag;
+        private int ar;
+
+        public Auto(string márka, string model, int evJarat, int km, string uzemAnyag, int ar)
+        {
+            this.márka = márka;
+            this.model = model;
+            this.evJarat = evJarat;
+            this.km = km;
+            this.uzemAnyag = uzemAnyag;
+            this.ar = ar;
+        }
+
+        public string Márka { get => márka; set => márka = value; }
+        public string Model { get => model; set => model = value; }
+        public int EvJarat { get => evJarat; set => evJarat = value; }
+        public int Km { get => km; set => km = value; }
+        public string UzemAnyag { get => uzemAnyag; set => uzemAnyag = value; }
+        public int Ar { get => ar; set => ar = value; }
+
+        public static int DarabSzam()
+        {
+            return lista.Count();
+        }
+
+        public double ToyotaAtlagAr()
+        {
+            Console.WriteLine();
+            double atlag = 0;
+            int counter = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (márka == "Toyota")
+                {
+                    Console.WriteLine("Ár: " + lista[i].ar);
+                    atlag += lista[i].ar;
+                    counter++;
+                }
+            }
+
+            return atlag / counter;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Első git repo-m.");
+            StreamReader sr = new StreamReader("auto.txt");
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                string[] adatok = sor.Split("\t".ToCharArray());
+                Auto temp = new Auto(adatok[0], adatok[1], int.Parse(adatok[2]), int.Parse(adatok[3]), adatok[4], int.Parse(adatok[5]));
+                Auto.lista.Add(temp);
+            }
+            sr.Close();
+
+            Console.WriteLine(Auto.DarabSzam());
+
+            Console.ReadLine();
         }
     }
 }
